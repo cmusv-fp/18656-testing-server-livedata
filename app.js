dfbafb4b3399a54e27160edae791b016ae4f3193
pallav100 <100pallav@gmail.com>
2023-12-07 13:19:00 -0800

    8000

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var http = require('http');
+var wsServer = require("ws").Server;
 
+var sockserver = new wsServer({ port: 8000 })
+sockserver.on('connection', ws => {
+  console.log("ll")
+  var a = {
+    'alal': 1,
+    "mmmm": 2,
+    "pol": "mmm"
+  };
+  ws.send(JSON.stringify(a));
+  ws.on('close', () => console.log('Client has disconnected!'))
+  // ws.on('message', data => {
+  //   // sockserver.clients.forEach(client => {
+  //     console.log(data.)
+  //     // client.send(`hello`)
+  // })
+  // // })
+  ws.onerror = function () {
+    console.log('websocket error')
+  }
+ })
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
